5073aa64f1f8945b10aedb2ec925d4f5d7e53129
richysleeks <49183245+richysleeks@users.noreply.github.com>
2019-12-27 02:38:43 +0100

    fff

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,11 +8,11 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(DepartmentSeeder::class);
         // $this->call(PositionSeeder::class);
-        $this->call(AdminUserSeeder::class);
+        // $this->call(AdminUserSeeder::class);
         // $this->call(CaseUserSeeder::class);
-        $this->call(RoleSeeder::class);
+        // $this->call(RoleSeeder::class);
         // $this->call(PermissionSeeder::class);
-        $this->call(UserPermissionSeeder::class);
+        // $this->call(UserPermissionSeeder::class);
         $this->call(UserSeeder::class);
         // $this->call(ReportedCaseSeeder::class);
         // $this->call(CaseHolderSeeder::class);
